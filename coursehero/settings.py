7325cf627419b79fda708e381d176c9895cc4688
Erickson Odianga <odiangaeric1997@gmail.com>
2021-04-27 10:09:35 +0300

    add static root

--- coursehero/settings.py
+++ coursehero/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'root_static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
